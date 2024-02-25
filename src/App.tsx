114c495fd1b533da44fc8b01013659d0b5770595
Shourov Foisal <shourovfoisal@gmail.com>
2024-02-25 12:30:52 +0600

    Rendered the basic table

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
 import "./App.css";
+import { mockData, tableHeaders } from "./mockData";
 
 function App() {
-  return <div></div>;
+  return (
+    <div className="table">
+      {tableHeaders.map((header) => {
+        return (
+          <div className="row" key={header.accessorKey}>
+            {mockData.map((data) => {
+              return (
+                <div className="cell" key={data["date"]}>
+                  {data[header.accessorKey as keyof typeof data]}
+                </div>
+              );
+            })}
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default App;
---
+++ src/mockData.ts
@@ -0,0 +1,54 @@
+export const tableHeaders = [
+  {
+    accessorKey: "date",
+    header: "\xa0",
+  },
+  {
+    accessorKey: "roomToSell",
+    header: "Room To Sell",
+  },
+  {
+    accessorKey: "roomSold",
+    header: "Room Sold",
+  },
+  {
+    accessorKey: "Plan 1",
+    header: "Plan 1",
+  },
+  {
+    accessorKey: "Plan 2",
+    header: "Plan 2",
+  },
+];
+
+export const mockData = [
+  {
+    date: "2024-01-30",
+    roomToSell: 2,
+    roomSold: 1,
+    "Plan 1":
+      "10000100001000010\n0001000010000100001000010000100001\n0000100004684321100001000010000100001000\n0100001000010000100001000010000100004684321",
+    "Plan 2": 20000,
+  },
+  {
+    date: "2024-01-31",
+    roomToSell: 4,
+    roomSold: 8,
+    "Plan 1": 10000,
+    "Plan 2": 35000,
+  },
+  {
+    date: "2024-02-01",
+    roomToSell: 3,
+    roomSold: 9,
+    "Plan 1": 5000,
+    "Plan 2": 10000,
+  },
+  {
+    date: "2024-02-02",
+    roomToSell: 6,
+    roomSold: 8,
+    "Plan 1": 25000,
+    "Plan 2": 46000,
+  },
+];
